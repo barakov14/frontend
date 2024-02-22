5eaeca5736efef64d2619b1da167b93c97012d62
barakov14 <playmagma4@gmail.com>
2024-02-22 21:28:38 +0600

    task

--- src/app/task/data-access/models/tasks.model.ts
+++ src/app/task/data-access/models/tasks.model.ts
@@ -1,24 +1,14 @@
-import {DeepReadonly} from '../../../utils/deep-readonly'
-
 export interface Task {
   id: number
   title: string
   description: string
   assignedDeveloperId: number
   deadline: Date
+  createdAt: Date
   status: 'IN_PROGRESS' | 'DONE'
 }
 
-export type TaskListDTO = DeepReadonly<{
-  tasks: [
-    id: number,
-    title: string,
-    description: string,
-    assignedDeveloperId: number,
-    deadline: Date,
-    status: 'IN_PROGRESS' | 'DONE',
-  ]
-}>
+export interface TaskList extends Array<Task> {}
 
 export interface CreateTask {
   title: string
--- src/app/task/tasks/tasks/tasks.component.ts
+++ src/app/task/tasks/tasks/tasks.component.ts
@@ -4,7 +4,7 @@ import {MatPaginator} from '@angular/material/paginator'
 import {TableModule} from 'primeng/table'
 import {ButtonModule} from 'primeng/button'
 import {AsyncPipe, NgForOf} from '@angular/common'
-import {TaskListDTO} from '../../data-access/models/tasks.model'
+import {TaskList} from '../../data-access/models/tasks.model'
 
 @Component({
   selector: 'app-tasks',
@@ -22,7 +22,7 @@ import {TaskListDTO} from '../../data-access/models/tasks.model'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TasksComponent {
-  @Input() tasks: TaskListDTO | undefined | null
+  @Input() tasks: TaskList | undefined | null
   public first = 0
   public rows = 10
 }
